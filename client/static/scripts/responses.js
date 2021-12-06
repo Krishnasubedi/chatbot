67967095bcd7db1ba2d0f99bf11f8b882e72565e
KrishnaMcc <mcc.chatbot.2021@gmail.com>
2021-12-06 17:55:03 +0900

    modified function with async and await

--- client/static/scripts/responses.js
+++ client/static/scripts/responses.js
@@ -1,19 +1,21 @@
-function getBotResponse(input) {
-    //rock paper scissors
-    if (input == "rock") {
-        return "paper";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
-    }
+//write code here to send request to dialog flow
 
-    // Simple responses
-    if (input == "hello") {
-        return "Hello there!";
-    } else if (input == "goodbye") {
-        return "Talk to you later!";
-    } else {
-        return "Try asking something else!";
-    }
+function getBotResponse(input) {
+  //rock paper scissors
+  if (input == "rock") {
+    return "paper";
+  } else if (input == "paper") {
+    return "scissors";
+  } else if (input == "scissors") {
+    return "rock";
+  }
+
+  // Simple responses
+  if (input == "hello") {
+    return "Hello there!";
+  } else if (input == "goodbye") {
+    return "Talk to you later!";
+  } else {
+    return "Try asking something else!";
+  }
 }
